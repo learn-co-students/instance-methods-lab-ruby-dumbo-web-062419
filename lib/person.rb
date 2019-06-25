35fc89e5352c28e65060fb51b2010d89b812d925
Aliciea Dawn Suechin <aliciea@Alicieas-MacBook-Pro.local>
2019-06-25 11:06:53 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def talk
+        puts "Hello World!"
+        def walk 
+            puts "The Person is walking"
+end
+end 
+end 
+alan_kay = Person.new
+alan_kay
+alan_kay.talk
+alan_kay
+alan_kay.walk 
+alan_kay
